d3823e91e7969abaae70cabd4c0a1151cc61c8f6
Lee Richardson <github@leerichardson.com>
2017-07-29 22:57:40 -0400

    An initial app, not much in it

---
+++ LpricharCodeHour/Views/RootViewController.cs
@@ -0,0 +1,63 @@
+﻿using UIKit;
+using Foundation;
+using LpricharCodeHour.Utils;
+
+namespace LpricharCodeHour.Views
+{
+    [Register("UniversalView")]
+    public class UniversalView : UIView
+    {
+        public UniversalView()
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            BackgroundColor = UIColor.Red;
+        }
+    }
+
+    [Register("RootViewController")]
+    public class RootViewController : UIViewController
+    {
+        private UILabel _label;
+
+        public RootViewController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            View = new UniversalView();
+
+            base.ViewDidLoad();
+
+            AddViews();
+            ConstrainLayout();
+        }
+
+        private void AddViews()
+        {
+            _label = AddLabel(View, "The lprichar Code Hour");
+        }
+
+        private static UILabel AddLabel(UIView parent, string text)
+        {
+            var label = new UILabel
+            {
+                Text = text
+            };
+            parent.AddSubview(label);
+            return label;
+        }
+
+        private void ConstrainLayout()
+        {
+            View.ConstrainLayout(() => 
+                _label.Frame.GetCenterX() == View.Frame.GetCenterX()
+                && _label.Frame.GetCenterY() == View.Frame.GetCenterY()
+            );
+        }
+    }
+}
